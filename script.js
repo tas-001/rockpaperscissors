04b1665a665734a2cfda14a72b46f7f66246261e
Mariyan Tashev <mtashev@vmware.com>
2020-07-27 19:48:35 +0300

    Fixed validation, + keeping score, better messages

--- script.js
+++ script.js
@@ -6,55 +6,72 @@ function computerPlay() {
     return pick;
 }
 
-let userPick = prompt("Please select rock, paper or scissors");
+let playerWins = "Player wins!";
+let computerWins = "Computer wins!";
+let equalGame = "You selected the same tool";
+let playerScore = 0;
+let computerScore = 0;
 
-function playerPlay(userPick) {
-    let userPickToLower = userPick.toLowerCase();
-    if (userPickToLower == "rock" || userPickToLower == "paper" || userPickToLower == "scissors") {
-        console.log("You entered: " + userPickToLower);
-        return userPickToLower;
-    } else {
-        return "User entered an invalid input";
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == "rock") {
+        if (computerSelection == "rock") {
+            return equalGame;
+        } else if (computerSelection == "paper") {
+            computerScore++;
+            return computerWins;
+        } else {
+            playerScore++;
+            return playerWins;
+        }
+    }
+    if (playerSelection == "paper") {
+        if (computerSelection == "paper") {
+            return equalGame;
+        } else if (computerSelection == "rock") {
+            playerScore++;
+            return playerWins;
+        } else {
+            computerScore++;
+            return computerWins;
+        }
+    }
+    if (playerSelection == "scissors") {
+        if (computerSelection == "scissors") {
+            return equalGame;
+        } else if (computerSelection == "rock") {
+            computerScore++;
+            return computerWins;
+        } else {
+            playerScore++;
+            return playerWins;
+        }
     }
 }
 
-let player = playerPlay(userPick);
-let computer = computerPlay();
-
-function playRound(player, computer) {
-    if (player == "rock") {
-        if (computer == "rock") {
-            return "Play again! You both selected rock";
-        } else if (computer == "paper") {
-            return "The computer wins. Paper beats rock";
-        } else {
-            return "You win. Rock beats scissors";
-        }
-    }
-    if (player == "paper") {
-        if (computer == "paper") {
-            return "Play again! You both selected paper";
-        } else if (computer == "rock") {
-            return "You win. Paper beats rock";
-        } else {
-            return "The computer wins. Scissors beat paper";
-        }
-    }
-    if (player == "scissors") {
-        if (computer == "scissors") {
-            return "Play again! You both selected scissors";
-        } else if (computer == "rock") {
-            return "The computer wins. Rock beats scissors";
-        } else {
-            return "You win. Scissors beat paper";
-        }
+function winner(computerScore, playerScore){
+    if (computerScore > playerScore) {
+        return "Computer is the winner. Try again!";
+    } else if (computerScore < playerScore) {
+        return "You won! Congrats!";
+    } else {
+        return "Equal game. Maybe try another time?";
     }
 }
 
 function game(rounds) {
     for (let index = 0; index < rounds; index++) {
-        userPick;
-        console.log(playRound(player, computer));
+        console.log("ROUND " + (Number(index) + Number(1)));
+        let playerSelection = prompt("Rock Paper Scissors");
+        if (playerSelection.toLowerCase() != "rock" && playerSelection.toLowerCase() != "paper" && playerSelection.toLowerCase() != "scissors") {
+            console.log("You entered invalid game input");
+        }
+        let computerSelection = computerPlay();
+        console.log(playRound(playerSelection, computerSelection));
     }
+    console.log("RESULT: ");
+    console.log("Computer " + computerScore + " : " + playerScore + " Player");
+    console.log(winner(computerScore, playerScore));
+    console.log("Thanks for playing!");
 }
 
+game(3);
